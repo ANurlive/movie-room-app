c75552ac2ee7da05a7d1f7d6f09ad3b6699e3b08
assel <kozhbanova.23@gmail.com>
2025-07-19 19:47:03 +0500

    test: add MovieList component test

--- src/components/EmptySearchResult/EmptySearchResult.tsx
+++ src/components/EmptySearchResult/EmptySearchResult.tsx
@@ -4,7 +4,7 @@ import { EMPTY_RESULT_MESSAGES } from './messages';
 export default class EmptySearchResult extends Component {
   render() {
     return (
-      <div className="messageContainer">
+      <div className="messageContainer" data-testid="empty-search-result">
         <h3 className="font-bold">{EMPTY_RESULT_MESSAGES.DEFAULT}</h3>
       </div>
     );
---
+++ src/components/MovieList/MovieList.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react';
+import type { MovieItem } from '../../types';
+import MovieList from './MovieList';
+
+jest.mock('../MovieCard/MovieCard', () => ({
+  __esModule: true,
+  default: jest.fn(() => <div data-testid="movie-card"></div>),
+}));
+
+const mockList: MovieItem[] = [
+  {
+    id: 1,
+    title: 'movie 1',
+    overview: 'overview 1',
+    releaseDate: '2025-07-19',
+    posterPath: '/poster1.jpg',
+  },
+  {
+    id: 2,
+    title: 'movie 2',
+    overview: 'overview 2',
+    releaseDate: '2025-07-20',
+    posterPath: '/poster2.jpg',
+  },
+];
+
+describe('MovieList component', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('renders EmptySearchResult when movieList is empty', () => {
+    render(<MovieList movieList={[]} />);
+    expect(screen.getByTestId('empty-search-result')).toBeInTheDocument();
+  });
+
+  test('renders a list of MovieCard component', () => {
+    render(<MovieList movieList={mockList} />);
+    const movieCards = screen.getByTestId('movie-card');
+    expect(movieCards).toHaveLength(mockList.length);
+  });
+});
